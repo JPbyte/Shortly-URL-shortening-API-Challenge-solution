17f56a8a1d2c85821601a6ca395ba4c19e94012c
joão pedro fernandes teixeira <pedro.fernandes1058@gmail.com>
2022-12-27 23:46:09 -0300

    scripts finalizados

--- src/script.js
+++ src/script.js
@@ -2,6 +2,10 @@
 const link = document.getElementById("link_put")
 const btnLink = document.getElementById("link_btn")
 const statsRotate = document.querySelectorAll(".img_border")
+const shortCamp = document.querySelector(".short_api")
+const shortLink = document.querySelector(".link_short")
+let short = document.querySelector(".shorted")
+
 
 statsRotate.forEach(st => {
     st.addEventListener("mouseenter", () => {        
@@ -11,5 +15,20 @@ statsRotate.forEach(st => {
 })
 
 btnLink.addEventListener("click", () => {
-    
+    let linkValue = link.value;
+    shortLink.innerHTML = linkValue
+    shortCamp.style.display = "flex"
+        fetch(`https://api.shrtco.de/v2/shorten?url=${linkValue}`)
+        .then(Response => Response.json())
+        .then(consumo => {
+            short.style.color = "cyan"
+            short.value = consumo.result.short_link
+        })
+})
+
+let copy = document.querySelector(".button_copy")
+
+copy.addEventListener("click", () => {
+    short.select()
+    document.execCommand("copy")
 })
