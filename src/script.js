79ef069a94e5b06166c3f9f95159dbbd251be4cb
joão pedro fernandes teixeira <pedro.fernandes1058@gmail.com>
2022-12-28 00:31:19 -0300

    adicionando responsividade

--- src/script.js
+++ src/script.js
@@ -16,8 +16,19 @@ statsRotate.forEach(st => {
 
 btnLink.addEventListener("click", () => {
     let linkValue = link.value;
+    let regex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+    let validate = regex.test(link.value)
+
     shortLink.innerHTML = linkValue
-    shortCamp.style.display = "flex"
+
+    if (validate) {
+        shortCamp.style.opacity = "1"
+        shortCamp.style.transition = "0.4s"
+        link.style.border = "0.17rem green solid"
+    } else {
+        shortCamp.style.opacity = "0"
+        link.style.border = "0.17rem red solid"
+    }
         fetch(`https://api.shrtco.de/v2/shorten?url=${linkValue}`)
         .then(Response => Response.json())
         .then(consumo => {
